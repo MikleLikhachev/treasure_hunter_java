8a7dbb0208d6adef0b3ccba36b565d568ee084e5
MikleLikhachev <miklelikhachev@gmail.com>
2023-05-06 19:21:01 +0300

    Password.java Удаление ненужных методов для фильтрации

--- src/main/java/com/treasure_hunter_java/dictionary/Password.java
+++ src/main/java/com/treasure_hunter_java/dictionary/Password.java
@@ -2,58 +2,11 @@ package com.treasure_hunter_java.dictionary;
 
 public class Password {
 
-    private int length;
+    private final int length;
 
     private int countUsed = 1;
 
-    private String password;
-
-    boolean isContainsDigits;
-
-    boolean isContainsCapitalLetters;
-
-    boolean isContainsSmallLetters;
-
-    boolean isContainsSpecialSign;
-
-    boolean isContainsSpace;
-
-    public boolean getContainsDigits(){
-
-        return password.matches("^\\d+$");
-    }
-
-    public boolean getOnlyContainsDigits(){
-        return password.matches("^[0-9]+$");
-    }
-
-    public boolean getContainsCapitalLetters(){
-
-        return password.matches(".*[A-Z].*");
-    }
-
-    public boolean getOnlyContainsCapitalLetters(){
-        return password.matches("^[A-Z]+$");
-    }
-
-    public boolean getContainsSmallLetters() {
-
-        return password.matches(".*[a-z].*");
-    }
-
-    public boolean getOnlyContainsSmallLetters(){
-
-        return password.matches("^[a-z]+$");
-    }
-
-    public boolean getContainsSpecialSign(){
-
-        return password.matches(".*[^a-zA-Z0-9].*");
-    }
-
-    public boolean getOnlyContainsSpecialSign(){
-        return password.matches("^[^a-zA-Z0-9]+$");
-    }
+    private final String password;
 
     public String getPassword(){
         return password;
